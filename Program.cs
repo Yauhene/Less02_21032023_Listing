c2e519ee22cdbe52205ebbaac45f8b4f8f193751
Yauheni Aviltsau <XOPEK-007@yandex.ru>
2023-03-22 08:19:23 +0300

    добавляем недостающее

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System.Diagnostics;
+
+
+int[] array = 100_000.Create()
+                .Fill(1, 10);
+// array.ConvertToStringAndPrintToTerminal();
+
+
+
+int m = 1_000;
+Stopwatch sw = new Stopwatch();
+sw.Start();
+
+int max = array.BadGetSum(m);
+sw.Stop();
+
+Console.WriteLine($"BadGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+sw.Reset();
+sw.Start();
+max = array.GoodGetSum(m);
+sw.Stop();
+Console.WriteLine($"GoodGetSum: {max} => {sw.ElapsedMilliseconds}");
+
+// Console.WriteLine(array.ConvertToString());
+
+
+
